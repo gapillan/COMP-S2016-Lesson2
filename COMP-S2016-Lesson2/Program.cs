7c4170ab2c56c43f3d730db0d046b3ecd4312ce9
gapillan <gapillan@my.centennialcollege.ca>
2016-05-17 13:44:22 -0400

    Added addYtoX method and associated test

--- COMP-S2016-Lesson2/Program.cs
+++ COMP-S2016-Lesson2/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
  * Date: May 17th, 2016
  * Date Modified: May 17th, 2016
  * Description: Advance Methods Demo for Lesson 2 
- * Version: 0.0.3 - Added addXandY method and associated test 
+ * Version: 0.0.4 - Added addYtoX method and associated test 
  */
 
 
@@ -38,14 +38,19 @@ namespace COMP_S2016_Lesson2
 
             result = addXandY(x, y);
             Console.WriteLine(result);
+            Console.WriteLine();
+
+            addYtoX(ref x, ref y);
+
+            Console.WriteLine(x);
         }
 
         /**
          * simple method to add two values 
          * 
          * @method addXandY
-         * @param {int} 
-         * @param {int}
+         * @param {int} firstNumber 
+         * @param {int} secondNumber
          */
 
         public static int addXandY(int firstNumber, int secondNumber) 
@@ -53,6 +58,19 @@ namespace COMP_S2016_Lesson2
             int result = 0;
             result = firstNumber + secondNumber;
             return result;
+        }
+
+        /**
+         * simple method to demonstrate pass by reference  with ref keyword 
+         * 
+         * @method addYtoX
+         * @param {ref int} X 
+         * @param {ref int} Y
+         */
+        public static int addYtoX(ref int X, ref int Y) 
+        {
+            X += Y;
+            return X;
         }
 
     }
